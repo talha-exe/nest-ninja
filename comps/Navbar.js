7f4f2f61f7d58db93ad32c68b6ed65106ca3e1c0
talha.exe <94534752+talha-exe@users.noreply.github.com>
2022-03-03 22:59:27 +0500

    Update Navbar.js

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const Navbar = () => {
     return ( <nav>
         <div className="logo">
-            <Link href="/" passHref><a><Image src="/logo_new.png" alt="Ninja List" width={128} height={77}/></a></Link>
+            <Link href="/" passHref><a><Image src="/logo_new.png" alt="Ninja List" width={100} height={100}/></a></Link>
         </div>
         <Link href="/"><a>Home</a></Link>
         <Link href="/about"><a>About</a></Link>
